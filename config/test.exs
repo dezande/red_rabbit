40e633f5e64553fbcd74185ed1bf15789b254a18
Vincent Corniere <vincentcorniere@MacBook-Pro-de-Dezande.local>
2024-06-29 12:48:48 +0200

    add new login

--- config/test.exs
+++ config/test.exs
@@ -6,8 +6,8 @@ import Config
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :red_rabbit, RedRabbit.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: "vincentcorniere",
+  password: "",
   hostname: "localhost",
   database: "red_rabbit_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
